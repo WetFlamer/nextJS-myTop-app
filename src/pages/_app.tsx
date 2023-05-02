4733602daab15cd17041374e866447c355535c39
WetFlamer <xamidchabaev04@gmail.com>
2023-05-02 16:53:19 +0300

    установлен NextJS + typescript + stylelint + eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,regular,500,700,900"
+          rel="stylesheet"
+        />{" "}
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
